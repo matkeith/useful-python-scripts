3b906c19cdaa4bf6e083de28146dd93f109d5973
Mathew Keith <mkeith@mbsi.com>
2021-10-18 10:19:16 -0700

    set default path. monit has problems sending parameters. replaces with command line argument if given

--- PathChecker.py
+++ PathChecker.py
@@ -2,12 +2,15 @@
 import os
 import sys
 
-#path = "/media/Colossus/Series/"
-path = sys.argv[1]
+path = "/media/Colossus/Series/"
+if len(sys.argv) > 1:
+        path = sys.argv[1]
 
 isFile = os.path.exists(path)
 #print(path)
 #print(isFile)
+#print(len(sys.argv))
+
 if isFile:
         sys.exit(0)
 else:
